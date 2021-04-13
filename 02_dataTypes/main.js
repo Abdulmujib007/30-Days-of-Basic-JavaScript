ed186a338f1094ce6b5e0e84f5cf738a79217dcf
Abdulmujib <asiyanbiakorede@gmail.com>
2021-04-13 12:09:42 +0100

    day 3

--- 02_dataTypes/main.js
+++ 02_dataTypes/main.js
@@ -4,7 +4,7 @@ console.log(challenge); // 30 days of javascript
 console.log(challenge.length); // 23
 console.log(challenge.toUpperCase()); // 30 days of javascript
 console.log(challenge.toLowerCase()); //30 DAYS OF JAVASCRIPT
-console.log(challenge.substr(3, 4)); // Days
+console.log(challenge.substring(3, 7)); // Days
 console.log(challenge.substr(3)); //  Days Of JavaScript
 console.log(challenge.includes("Script")); // true
 console.log(challenge.split(" ")); // ["30","Days","Of","JavaScript"]
@@ -13,7 +13,7 @@ console.log(
 ); // ["Facebook","Google","Microsoft","Apple","IBM","oracle","Amazon.",]
 console.log(challenge.replace("JavaScript", "Phython")); // phython
 console.log(challenge.charAt(15)); // s
-console.log(challenge.charCodeAt("J")); // 51
+/console.log(challenge.charCodeAt("J")); // 51
 console.log(challenge.indexOf("a")); // 4
 console.log(challenge.lastIndexOf("a")); // 14
 
@@ -35,7 +35,7 @@ console.log(
 console.log(challenge.trim()); //30 Days Of JavaScript
 console.log(challenge.startsWith("")); //true
 console.log(challenge.endsWith("")); //true
-console.log(challenge.match("a"));
+console.log(challenge.match(/a/gi));
 console.log("30 days of ".concat("Javascript")); // 30 Days Of JavaScript
 console.log(challenge.repeat("20"));
 //exercise 2
@@ -45,14 +45,14 @@ console.log(
 console.log(
   `"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."`
 );
-console.log(typeof "10" == 10);
-console.log(parseInt("10") === 10);
+console.log(typeof "10" === typeof 10);
+console.log(typeof parseInt("10") ===  typeof 10);
 console.log(parseFloat("9.8"));
 console.log(Math.round("9.8"));
 console.log("phython".includes("on"));
 console.log("jargon".includes("on"));
 console.log("I hope this course is not full of jargon".includes("jargon"));
-console.log(Math.round(Math.random() * 100));
+console.log(Math.floor(Math.random() * 101));
 console.log(Math.round(Math.random() * (100 - 50) + 50));
 console.log(Math.round(Math.random() * 225));
 console.log(1, 1, 1, 1, 1);
@@ -72,12 +72,12 @@ console.log(java[Math.floor(Math.random() * java.length)]);
 // exercise 3
 console.log(
   "'Love is the best thing in this world. Some found their love and some are still looking for their love.'".match(
-    "love"
+    /love/gi
   )
 );
-console.log('You cannot end a sentence with because because because is a conjunction'.match("because"));
+console.log('You cannot end a sentence with because because because is a conjunction'.match(/because/gi));
 const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
-console.log(sentence.match())
+console.log(sentence.replace(/[^a-zA-Z ]/gi, ""));
 let add = (5000 + 15000) * 12;
 console.log(add);
 let annual = add + 10000;
---
+++ 03/main.js
@@ -0,0 +1,157 @@
+//exercise 1
+let firstName = "Abdulmujib";
+let lastName = "asiyanbi";
+let country = "hustling for a better one";
+let age = 202;
+let isMarried = true;
+let year = 2021;
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof year);
+console.log(parseInt(9.8) == 10);
+console.log(5 > 3 && 10 > 7);
+console.log(5 > 4 || 10 < 2);
+console.log(5 === 5);
+console.log(5 > 3 && 10 < 7);
+console.log(5 < 4 || 10 < 2);
+console.log(5 !== 5);
+console.log(4 > 3); //true
+console.log(4 >= 3); //true
+console.log(4 < 3); //false
+console.log(4 <= 3); // false
+console.log(4 == 4); // true
+console.log(4 === 4); // true
+console.log(4 != 4); // false
+console.log(4 !== 4); // false
+console.log(4 != "4"); // false
+console.log(4 == "4"); //true
+console.log(4 === "4"); // false
+console.log(4 > 3 && 10 < 12); //true
+console.log(4 > 3 && 10 > 12); //false
+console.log(4 > 3 || 10 < 12); //true
+console.log(4 > 3 || 10 > 12); // true
+console.log(!(4 > 3)); // false
+console.log(!(4 < 3)); // true
+console.log(!false); //true
+console.log(!(4 > 3 && 10 < 12)); // false
+console.log(!(4 > 3 && 10 > 12)); // true
+console.log(!(4 === "4")); // true
+console.log(!("python".includes("on") == "dragon".includes("on"))); //false
+const now = new Date();
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getTime());
+     //exercise 2
+let b = prompt("base"),
+  h = prompt("height");
+let area = 0.5 * b * h;
+console.log(area);
+
+let a = parseInt(prompt("Enter A"));
+let bb = parseInt(prompt("Enter B"));
+let c = parseInt(prompt("Enter C"));
+let p = a + bb + c;
+console.log(p);
+   
+let length = parseInt(prompt("enter length"));
+let width = parseInt(prompt("enter width"));
+let rectangleArea = length * width;
+console.log(rectangleArea);
+
+let rectanglePerimeter = 2 * (length * width);
+console.log(rectanglePerimeter);
+
+let radius = parseInt(prompt("enter radius"));
+let PI = 3.14;
+let circleArea = PI * radius * radius;
+console.log(circleArea) ;
+
+let circumf = 2 * PI * radius;
+console.log(circumf);
+let x1 = 2,
+    x2 = 6;
+let y1 = 2,
+    y2 = 10;
+let _x = x2 - x1;
+let _y = y2 - y1;
+let m = _y / _x;
+console.log(m);
+let xx1 = 0,
+    xx2 = 1;
+let yy1 = -2,
+    yy2 = 0;
+let _xx = xx2 - xx1;
+let _yy = yy2 -yy1;
+let mm = _yy / _xx;
+console.log(mm);
+console.log( m === mm); // true
+
+let yEnd = 0;
+  let xFirst = 3;
+  let xSecond = -3;
+  let yFirst = xFirst **2 + 6 * xFirst + 9;
+  let ySecond = xSecond **2 + 6 * xSecond + 9;
+  if (yFirst === 0){
+    console.log(`At X equals ${xFirst}, "Y" is equal to ${yEnd}` );
+  } else{
+    console.log("not equal");
+  }
+
+  if(ySecond === 0){
+    console.log(`At X equals ${xSecond},"Y" is equal to ${yEnd} `);
+  } else{
+    console.log("not equal");
+  }
+
+let hour = parseInt(prompt("enter hour"));
+let perHour = parseInt(prompt("enter per hour"));
+let multip = hour * perHour;
+console.log(multip); 
+
+let yourName = "Abdulmujib";
+if(yourName.length > 7){
+  console.log("Your name is long");
+
+} else{
+  console.log("your name is short");
+}
+let fiirstName = 'Asabeneh'
+let laastName = 'Yetayeh'
+if(fiirstName.length > laastName.legth){
+  console.log("Your first name ,asabenah is longer than your last name yetayeheyeh");
+}
+
+let myAge = 250;
+let yourAge = 25;
+ if(myAge > yourAge){
+   console.log("Am 225 years older than you.");
+ }
+  let birthYear = parseInt(prompt("enter birth year"));
+  let currentYear = 2021;
+  let differ = currentYear - birthYear;
+  let remain = 18 - differ;
+  if(birthYear <= 2003){
+    console.log(`You are ${differ}. You are old enough to drive `);
+  } else{
+    console.log(`you are ${differ}. you will be allowed to drive after ${remain} years. `);
+  }
+  numberYears = parseInt(prompt("Enter number of years"));
+  calculate = numberYears * 31536000 ;
+  console.log(`you have lived ${calculate} seconds.`);
+
+  date = new Date;
+  console.log(`${date.getFullYear()}-${ "0" + date.getMonth(0)}-${date.getDate(0)} ${ "0" + date.getHours(0)}:${ "0" + date.getMinutes(0)} `);
+
+  console.log(`${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()} `);
+
+  console.log(`${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()} `);
+          // exeercise 3
+  console.log(`${date.getFullYear() + "0" }-${ "0" + date.getMonth(0)}-${date.getDate(0)} ${ "0" + date.getHours(0)}:${ "0" + date.getMinutes(0)} `);
