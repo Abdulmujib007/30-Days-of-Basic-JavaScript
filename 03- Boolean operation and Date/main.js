4ac9c2e75a6a8d316d795b1050407c61a95db7fc
Abdulmujib <asiyanbiakorede@gmail.com>
2021-04-14 10:53:24 +0100

    Day 4 - conditionals'

--- 03- Boolean operation and Date/main.js
+++ 03- Boolean operation and Date/main.js
@@ -49,7 +49,7 @@ console.log(now.getDay());
 console.log(now.getHours());
 console.log(now.getMinutes());
 console.log(now.getTime());
-     //exercise 2
+//exercise 2
 let b = prompt("base"),
   h = prompt("height");
 let area = 0.5 * b * h;
@@ -60,98 +60,116 @@ let bb = parseInt(prompt("Enter B"));
 let c = parseInt(prompt("Enter C"));
 let p = a + bb + c;
 console.log(p);
-   
+
 let length = parseInt(prompt("enter length"));
 let width = parseInt(prompt("enter width"));
 let rectangleArea = length * width;
 console.log(rectangleArea);
 
-let rectanglePerimeter = 2 * (length * width);
+let rectanglePerimeter = 2 * (length + width);
 console.log(rectanglePerimeter);
 
 let radius = parseInt(prompt("enter radius"));
 let PI = 3.14;
 let circleArea = PI * radius * radius;
-console.log(circleArea) ;
+console.log(circleArea);
 
 let circumf = 2 * PI * radius;
 console.log(circumf);
 let x1 = 2,
-    x2 = 6;
+  x2 = 6;
 let y1 = 2,
-    y2 = 10;
+  y2 = 10;
 let _x = x2 - x1;
 let _y = y2 - y1;
 let m = _y / _x;
 console.log(m);
 let xx1 = 0,
-    xx2 = 1;
+  xx2 = 1;
 let yy1 = -2,
-    yy2 = 0;
+  yy2 = 0;
 let _xx = xx2 - xx1;
-let _yy = yy2 -yy1;
+let _yy = yy2 - yy1;
 let mm = _yy / _xx;
 console.log(mm);
-console.log( m === mm); // true
+console.log(m === mm); // true
 
 let yEnd = 0;
-  let xFirst = 3;
-  let xSecond = -3;
-  let yFirst = xFirst **2 + 6 * xFirst + 9;
-  let ySecond = xSecond **2 + 6 * xSecond + 9;
-  if (yFirst === 0){
-    console.log(`At X equals ${xFirst}, "Y" is equal to ${yEnd}` );
-  } else{
-    console.log("not equal");
-  }
+let xFirst = 3;
+let xSecond = -3;
+let yFirst = xFirst ** 2 + 6 * xFirst + 9;
+let ySecond = xSecond ** 2 + 6 * xSecond + 9;
+if (yFirst === 0) {
+  console.log(`At X equals ${xFirst}, "Y" is equal to ${yEnd}`);
+} else {
+  console.log("not equal");
+}
 
-  if(ySecond === 0){
-    console.log(`At X equals ${xSecond},"Y" is equal to ${yEnd} `);
-  } else{
-    console.log("not equal");
-  }
+if (ySecond === 0) {
+  console.log(`At X equals ${xSecond},"Y" is equal to ${yEnd} `);
+} else {
+  console.log("not equal");
+}
 
 let hour = parseInt(prompt("enter hour"));
 let perHour = parseInt(prompt("enter per hour"));
 let multip = hour * perHour;
-console.log(multip); 
+console.log(multip);
 
 let yourName = "Abdulmujib";
-if(yourName.length > 7){
+if (yourName.length > 7) {
   console.log("Your name is long");
-
-} else{
+} else {
   console.log("your name is short");
 }
-let fiirstName = 'Asabeneh'
-let laastName = 'Yetayeh'
-if(fiirstName.length > laastName.legth){
-  console.log("Your first name ,asabenah is longer than your last name yetayeheyeh");
+let fiirstName = "Asabeneh";
+let laastName = "Yetayeh";
+if (fiirstName.length > laastName.legth) {
+  console.log(
+    "Your first name ,asabenah is longer than your last name yetayeheyeh"
+  );
 }
 
 let myAge = 250;
 let yourAge = 25;
- if(myAge > yourAge){
-   console.log("Am 225 years older than you.");
- }
-  let birthYear = parseInt(prompt("enter birth year"));
-  let currentYear = 2021;
-  let differ = currentYear - birthYear;
-  let remain = 18 - differ;
-  if(birthYear <= 2003){
-    console.log(`You are ${differ}. You are old enough to drive `);
-  } else{
-    console.log(`you are ${differ}. you will be allowed to drive after ${remain} years. `);
-  }
-  numberYears = parseInt(prompt("Enter number of years"));
-  calculate = numberYears * 31536000 ;
-  console.log(`you have lived ${calculate} seconds.`);
+if (myAge > yourAge) {
+  console.log("Am 225 years older than you.");
+}
+let birthYear = parseInt(prompt("enter birth year"));
+let currentYear = 2021;
+let differ = currentYear - birthYear;
+let remain = 18 - differ;
+if (birthYear <= 2003) {
+  console.log(`You are ${differ}. You are old enough to drive `);
+} else {
+  console.log(
+    `you are ${differ}. you will be allowed to drive after ${remain} years. `
+  );
+}
+numberYears = parseInt(prompt("Enter number of years"));
+calculate = numberYears * 31536000;
+console.log(`you have lived ${calculate} seconds.`);
 
-  date = new Date;
-  console.log(`${date.getFullYear()}-${ "0" + date.getMonth(0)}-${date.getDate(0)} ${ "0" + date.getHours(0)}:${ "0" + date.getMinutes(0)} `);
+date = new Date();
+console.log(
+  `${date.getFullYear()}-${+date.getMonth(0)}-${date.getDate(
+    0
+  )} ${date.getHours(0)}:${date.getMinutes(0)} `
+);
 
-  console.log(`${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()} `);
+console.log(
+  `${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()} `
+);
 
-  console.log(`${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()} `);
-          // exeercise 3
-  console.log(`${date.getFullYear() + "0" }-${ "0" + date.getMonth(0)}-${date.getDate(0)} ${ "0" + date.getHours(0)}:${ "0" + date.getMinutes(0)} `);
+console.log(
+  `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()} `
+);
+// exeercise 3
+console.log(
+  `${date.getFullYear()}-${
+    date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth()
+  }-${date.getDate() < 10 ? "0" + date.getDate() : date.getDat()} ${
+    date.getHours() < 10 ? "0" + date.getHours() : date.getHours()
+  }:${date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()}`
+);
+z
---
+++ 04_conditionals/main.js
@@ -0,0 +1,115 @@
+const enterAge = parseInt(prompt("Ente your Age."));
+if (enterAge >= 18) {
+  console.log("You are old enough to drive");
+} else {
+  console.log(`Your are left with ${18 - enterAge} years to drive.`);
+}
+const myAge = 25;
+const yourAge = parseInt(prompt("Enter your Age again"));
+
+if (myAge === yourAge) {
+  console.log("We be age mate mafo");
+} else if (myAge > yourAge) {
+  console.log(`Am ${myAge - yourAge} years older than you man `);
+} else {
+  console.log(`You are ${yourAge - myAge} years older than me.`);
+}
+let a = 4;
+let b = 3;
+if (a > b) {
+  console.log("a is greater than b");
+} else {
+  console.log("a is less than b");
+}
+console.log(a > b ? "a is greater than b" : "a is less than b");
+
+let number = parseInt(prompt("Enter a number"));
+if (number % 2 == 0) {
+  console.log(number + " is an even number");
+} else if (number % 2 == 1) {
+  console.log(number + " is an odd number");
+}
+let numb = parseInt(prompt("enter score"));
+if (numb == 0 || numb <= 49) {
+  console.log("F");
+} else if (numb == 50 || numb <= 59) {
+  console.log("D");
+} else if (numb == 60 || numb <= 69) {
+  console.log("C");
+} else if (numb == 70 || numb <= 79) {
+  console.log("B");
+} else if (numb == 80 || numb <= 100) {
+  console.log("A");
+}
+let season = prompt("enter month").toLowerCase();
+if (season == "september" || season == "november" || season == "october") {
+  console.log("Autumn");
+} else if (
+  season == "december" ||
+  season == "january" ||
+  season == "feburary"
+) {
+  console.log("season is Winter");
+} else if (season == "march" || season == "april" || season == "may") {
+  console.log("season is Spring");
+} else if (season == "july" || season == "june" || season == "august") {
+  console.log("season is Summer");
+} else {
+  console.log("Pls use lowercase");
+}
+const today = prompt("Enter day").toLowerCase();
+switch (today) {
+  case "saturday":
+  case "sunday":
+    console.log("Saturday is a weekend");
+    break;
+  case "monday":
+  case "tuesday":
+  case "wednesday":
+  case "thursday":
+  case "friday":
+    console.log("This is a week day");
+    break;
+  default:
+    console.log("you writing in the rubbish");
+    break;
+}
+let month = prompt("Enter Month").toLowerCase();
+switch (month) {
+  case "january":
+    console.log("January has 28 days");
+    break;
+  case "february":
+    console.log("february has 28 days.");
+    break;
+  case "march":
+    console.log("March has 31 days.");
+    break;
+  case "april":
+    console.log("April has 30 days.");
+    break;
+  case "may":
+    console.log("May has 31 days");
+    break;
+  case "june":
+    console.log("June has 30 days.");
+    break;
+  case "july":
+    console.log("July has 31 days.");
+    break;
+  case "august":
+    console.log("August has 31 days");
+    break;
+  case "september":
+    console.log("September has 30 days.");
+    break;
+  case "october":
+    console.log("October has 31 days.");
+    break;
+  case "november":
+    console.log("November has 30 days.");
+    break;
+  case "december":
+    console.log("December has 31 days.");
+    break;
+}
