cd715469b5c581fe6808dd922dd787e66f6458e7
Abdulmujib <asiyanbiakorede@gmail.com>
2021-04-16 16:19:18 +0100

    day6 WIP

---
+++ 06_loop/main.js
@@ -0,0 +1,29 @@
+// for(let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+// let ii = 0;
+// while(ii <= 10){
+//     console.log(ii);
+//     ii++;
+// }
+
+// let iii = 0;
+// do{
+//     console.log(iii);
+//     iii++;
+// } while(iii <= 10 )
+
+for(let i = 10; i >= 0; i--){
+    console.log(i);
+}
+let ii = 10;
+while(ii >= 0){
+    console.log(ii);
+    ii--;
+}
+
+let iii = 10;
+do{
+    console.log(iii);
+    iii--;
+} while(iii >= 0 )
